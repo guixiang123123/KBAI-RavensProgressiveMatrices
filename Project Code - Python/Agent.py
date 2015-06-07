9603a6bd164ba1aaaef8bc96a75fe080e49aeea4
Ryan Cook <cyanrook@gmail.com>
2015-06-07 09:17:58 -0700

    First updates

--- Project Code - Python/Agent.py
+++ Project Code - Python/Agent.py
@@ -44,4 +44,82 @@ class Agent:
     # Make sure to return your answer *as an integer* at the end of Solve().
     # Returning your answer as a string may cause your program to crash.
     def Solve(self,problem):
-        return -1
+	# problem.name - string containing the name of the problem
+	# problem.problemType - string containing the type of problem
+	# problem.hasVisual - boolean on if pictures included
+	# problem.hasVerbal - boolean on if text included
+	# problem.figures - dictionary for figures & solutions
+
+        # Get all the objects in a figure (if verbal)
+        def GetVerbalObjs(Figure):
+            Objs = []
+            for obj in list(Figure.objects):
+                Objs.append([Figure.objects[obj].name, Figure.objects[obj].attributes])
+            return Objs
+
+        # Calculate the difference between two objects in regards to attributes
+        def CalcObjDelta(Obj1, Obj2):
+            Value = 0
+            Delta = {}
+            Attr1 = list(Obj1)
+            Attr2 = list(Obj2)
+            for Attr in Attr1:
+                if Attr in Obj2:
+                    if Obj1[Attr] != Obj2[Attr]:
+                        Value += 1
+                        Delta[Attr]=[Obj1[Attr],Obj2[Attr]]
+                else:
+                    Value+=1
+                    Delta[Attr]=[Obj1[Attr],None]
+            for Attr in Attr2:
+                if Attr not in Obj1:
+                    Value+=1
+                    Delta[Attr]=[None,Obj2[Attr]]
+            return [Value, Delta]
+
+        # Match up objects between two different figures
+        def FigureObjMatch(Fig1, Fig2):
+            Objs1 = GetVerbalObjs(Fig1)
+            Objs2 = GetVerbalObjs(Fig2)
+            return None
+
+        if problem.problemType == "2x2":
+            TopLeft = problem.figures['A']
+            TopRight = problem.figures['B']
+            BotLeft = problem.figures['C']
+            Solutions = [problem.figures['1'], problem.figures['2'],
+	    	             problem.figures['3'], problem.figures['4'],
+	                     problem.figures['5'], problem.figures['6']]
+            if problem.hasVerbal:
+                TLO = GetVerbalObjs(TopLeft)
+                TRO = GetVerbalObjs(TopRight)
+                BLO = GetVerbalObjs(BotLeft)
+                SLO = []
+                for Sol in Solutions:
+                    SLO.append(GetVerbalObjs(Sol))
+                print problem.name
+                print TLO
+                print TRO
+                [V,D] = CalcObjDelta(TLO[0][1],TRO[0][1])
+                print V
+                print D
+
+
+        if problem.problemType == "3x3":
+            TopLeft = problem.figures['A']
+            TopCenter = problem.figures['B']
+            TopRight = problem.figures['C']
+            MidLeft = problem.figures['D']
+            MidCenter = problem.figures['E']
+            MidRight = problem.figures['F']
+            BotLeft = problem.figures['G']
+            BotCenter = problem.figures['H']
+            Solutions = [problem.figures['1'], problem.figures['2'],
+	   		     problem.figures['3'], problem.figures['4'],
+			     problem.figures['5'], problem.figures['6'],
+			     problem.figures['7'], problem.figures['8']]
+
+        BestGuess = 1
+        ActualSolution = problem.checkAnswer(int(BestGuess))
+        return BestGuess
+
